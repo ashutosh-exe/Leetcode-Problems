2a0b417b6bde12aacc90c24adbc294cee72f9ba4
Ashutosh Singh <ashutoshsingh200427@gmail.com>
2026-09-24 07:28:24 +0530

    Time: 1 ms (83.14%), Space: 42.8 MB (83.29%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        for(char i='a';i<='z';i++){
-            if(sentence.indexOf(i)==-1){
+        boolean[] arr = new boolean[26];
+        for(char c:sentence.toCharArray()){
+            arr[c-'a'] = true;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]==false){
                 return false;
             }
         }
